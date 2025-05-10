fbd69db2de757678f94566cc514ea6af3c173894
PHTMGatt <phtmgatt@gmail.com>
2025-05-10 17:04:43 -0400

    Final Working Version

--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -1,6 +1,8 @@
 import { gql } from '@apollo/client'
 
 
+// Note removed user{} within the getMe query. This fixed the savedBooks page
+
 export const GET_ME = gql`
     query getMe{
         getMe {  
